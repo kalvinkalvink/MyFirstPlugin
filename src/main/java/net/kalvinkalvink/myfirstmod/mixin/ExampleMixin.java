606f2dc79525947b71d2ecf6e40d297be54ddf91
kalvinkalvink <choi4a06@gmail.com>
2022-09-24 21:12:15 +0800

    renamed frabic example mod

--- src/main/java/net/kalvinkalvink/myfirstmod/mixin/ExampleMixin.java
+++ src/main/java/net/kalvinkalvink/myfirstmod/mixin/ExampleMixin.java
@@ -1,6 +1,5 @@
-package net.fabricmc.example.mixin;
-
-import net.fabricmc.example.ExampleMod;
+package net.kalvinkalvink.myfirstmod.mixin;
+import net.kalvinkalvink.myfirstmod.MyFirstMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +10,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		MyFirstMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
